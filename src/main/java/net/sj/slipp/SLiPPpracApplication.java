2ab0860bc91a13de25611655477e3b11772949ce
laura <sjngjang@gmail.com>
2017-07-20 08:58:36 +0100

    initial commit

---
+++ src/main/java/net/sj/slipp/SLiPPpracApplication.java
@@ -0,0 +1,12 @@
+package net.sj.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SLiPPpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SLiPPpracApplication.class, args);
+	}
+}
